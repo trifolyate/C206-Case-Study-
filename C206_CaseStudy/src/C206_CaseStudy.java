0ed8a9a3ec376402a4490bd8c7d09613c28a54ec
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-26 14:50:53 +0800

    Zhuo hong - updated main code format for easier coding for members

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
-	private static final int OPTION_QUIT = 3;
+	private static final int OPTION_QUIT = 4;
 	private static final int OPTION_VIEW = 1;
 	private static final int OPTION_ADD = 2;
+	private static final int OPTION_DELETE = 3;
 
 	public static void itemTypeMenu() {
 		C206_CaseStudy.setHeader("users");
@@ -16,7 +17,7 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		ArrayList<student> studentList = new ArrayList<student>();
 		ArrayList<parent> parentList = new ArrayList<parent>();
-		//ArrayList<teacher> teacherList = new ArrayList<teacher>();
+		ArrayList<teacher> teacherList = new ArrayList<teacher>();
 		
 		int option = 0;
 
@@ -27,8 +28,10 @@ public class C206_CaseStudy {
 			if (option == OPTION_VIEW) {
 				// View all items
 				//C206_CaseStudy.viewAllStudent(studentList);
-				//C206_CaseStudy.viewAllChromebook(parentList);
+				//C206_CaseStudy.viewAllParent(parentList);
+				//C206_CaseStudy.viewAllTeacher(teacherList);
 			}
+			
 			else if (option == OPTION_ADD) {
 				// Add a new item
 				C206_CaseStudy.setHeader("ADD");			
@@ -40,23 +43,56 @@ public class C206_CaseStudy {
 					// Add a student
 					//student student = inputStudent();
 					//C206_CaseStudy.addStudent(studentList, student);
-					System.out.println("Camcorder added");
+					//System.out.println("Student added");
 
 				} else if (itemType == 2) {
 					// Add a parent
 					//parent parent = inputParent();
-					//C206_CaseStudy.addChromebook(parentList, parent);
-					System.out.println("Chromebook added");
-
+					//C206_CaseStudy.addParent(parentList, parent);
+					//System.out.println("Parent added");
+					
+				} else if (itemType == 3) {
+					// Add a teacher
+					//teacher teacher = inputTeacher();
+					//C206_CaseStudy.addTeacher(teacherList, teacher);
+					//System.out.println("teacher added");
 				} else {
 					System.out.println("Invalid type");
-			}} else if (option == OPTION_QUIT) {
+					}
+				}
+			
+				else if (option == OPTION_DELETE) {
+					C206_CaseStudy.setHeader("DELETE");			
+					C206_CaseStudy.itemTypeMenu();
+					
+					int itemType = Helper.readInt("Enter option to select user type > ");
+
+					if (itemType == 1) {
+						// Delete a student			
+						//System.out.println("Student deleted");
+
+					} else if (itemType == 2) {
+						// Delete a parent					
+						//System.out.println("Parent deleted");
+						
+					} else if (itemType == 3) {
+						// Delete a teacher						
+						//System.out.println("teacher deleted");
+					} else {
+						System.out.println("Invalid type");
+						}
+				
+				} 
+			
+				else if (option == OPTION_QUIT) {
 					System.out.println("Bye!");
-				} else {
+				}
+				else {
 					System.out.println("Invalid option");
 				}
 
 			}
+		
 		
 	}
 
@@ -64,7 +100,8 @@ public class C206_CaseStudy {
 		C206_CaseStudy.setHeader("RESOURCE CENTRE APP");
 		System.out.println("1. View user");
 		System.out.println("2. Add user");
-		System.out.println("3. Quit");
+		System.out.println("3. Delete user");
+		System.out.println("4. Quit");
 		Helper.line(80, "-");
 
 	}
