5be5060b1e437832a79f3e10c66ac1fd6d6c5237
trifolyate <21008946@myrp.edu.sg>
2022-08-02 23:56:40 +0800

    reworked the whole system and have changed both parent and student to make our lives easier

--- C206_CaseStudy/src/parent.java
+++ C206_CaseStudy/src/parent.java
@@ -1,52 +1,64 @@
 
-public class parent extends student{
+public class parent extends student {
 	private String parentName;
 	private String CCAid;
 	private String email;
 	private int contactNo;
-	
-	public parent(String studentID, String name, char grade, String classroom, String teacher, String parentName,
-			String cCAid, String email, int contactNo) {
-		super(studentID, name, grade, classroom, teacher);
+	private String FamilyID; // in case have two child, same family ID means belongs to the parent, so when
+								// logging in, can choose which student will the parent use
+
+	public parent(String studentID, String name, char grade, String classroom, String teacher, String cCA1, String cCA2,
+			String cCA3, String parentName, String cCAid, String email, int contactNo, String familyID) {
+		super(studentID, name, grade, classroom, teacher, cCA1, cCA2, cCA3);
 		this.parentName = parentName;
-		this.CCAid = cCAid;
+		CCAid = cCAid;
 		this.email = email;
 		this.contactNo = contactNo;
+		FamilyID = familyID;
 	}
 
-	public String toString() {
-		return String.format("%-10s %-30s %-10s %-10s", getName(), getCCAid(), getEmail(), getContact());
-	}
-
-	public String getName() {
+	public String getParentName() {
 		return parentName;
 	}
-	
-	public void setName(String parentName) {
+
+	public void setParentName(String parentName) {
 		this.parentName = parentName;
 	}
-	
+
 	public String getCCAid() {
 		return CCAid;
 	}
-	
-	public void setPassword(String CCAid) {
-		this.CCAid = CCAid;
+
+	public void setCCAid(String cCAid) {
+		CCAid = cCAid;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	public int getContact() {
+
+	public int getContactNo() {
 		return contactNo;
 	}
-	
-	public void setContact(int contactNo) {
+
+	public void setContactNo(int contactNo) {
 		this.contactNo = contactNo;
 	}
+
+	public String getFamilyID() {
+		return FamilyID;
+	}
+
+	public void setFamilyID(String familyID) {
+		FamilyID = familyID;
+	}
+
+	public String toString() {
+		return String.format("%-10s %-30s %-10s %-10s", getName(), getCCAid(), getEmail(), getContactNo());
+	}
+
 }
--- C206_CaseStudy/src/student.java
+++ C206_CaseStudy/src/student.java
@@ -5,28 +5,31 @@ public class student {
 	private char grade;
 	private String classroom;
 	private String teacher;
+	private String CCA1;
+	private String CCA2;
+	private String CCA3;
 
 	
-
-	public student(String studentID, String name, char grade, String classroom, String teacher) {
-		super();
+	
+	public student(String studentID, String name, char grade, String classroom, String teacher, String cCA1,
+			String cCA2, String cCA3) {
 		this.studentID = studentID;
 		this.name = name;
 		this.grade = grade;
 		this.classroom = classroom;
 		this.teacher = teacher;
+		CCA1 = cCA1;
+		CCA2 = cCA2;
+		CCA3 = cCA3;
 	}
-	
-	
+
 	public String getStudentID() {
 		return studentID;
 	}
 
-
 	public void setStudentID(String studentID) {
 		this.studentID = studentID;
 	}
-
 
 	public String getName() {
 		return name;
