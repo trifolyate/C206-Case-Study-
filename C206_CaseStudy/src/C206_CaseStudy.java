d49e2163d671bc8516d87ecc1b1d4b23a3f079ec
lee zhuo hong <21018193@myrp.edu.sg>
2022-08-02 15:47:32 +0800

    test

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -22,12 +22,13 @@ public class C206_CaseStudy {
 		ArrayList<Teacher> teacherList = new ArrayList<Teacher>();
 		ArrayList<CCA> CCAList = new ArrayList<CCA>();
 		ArrayList<CCACategory> categoryList = new ArrayList<CCACategory>();
-		
+
 		studentList.add(new student(1001, "John", 'B', "B101", "Mr Tan"));
 		studentList.add(new student(1002, "Max", 'C', "B102", "Mr Lim"));
-		parentList.add(new parent(1001, "John", 'B', "B101", "Mr Tan", "Mrs Lim", "B2345E", "limsy@gmail.com", 92345678));
+		parentList
+				.add(new parent(1001, "John", 'B', "B101", "Mr Tan", "Mrs Lim", "B2345E", "limsy@gmail.com", 92345678));
 		parentList.add(new parent(1002, "Max", 'C', "B102", "Mr Lim", "Mr lee", "A1234D", "leejh@gmail.com", 81234567));
-		
+
 		int option = 0;
 
 		while (option != OPTION_QUIT) {
@@ -36,79 +37,75 @@ public class C206_CaseStudy {
 
 			if (option == OPTION_VIEW) {
 				// View all items
-				//C206_CaseStudy.viewAllStudent(studentList);
+				// C206_CaseStudy.viewAllStudent(studentList);
 				C206_CaseStudy.viewAllParent(parentList);
-				//C206_CaseStudy.viewAllTeacher(teacherList);
-				
+				// C206_CaseStudy.viewAllTeacher(teacherList);
+
 			}
-			
+
 			else if (option == OPTION_ADD) {
 				// Add a new item
-				C206_CaseStudy.setHeader("ADD");			
+				C206_CaseStudy.setHeader("ADD");
 				C206_CaseStudy.itemTypeMenu();
-				
+
 				int userType = Helper.readInt("Enter option to select user type > ");
 
 				if (userType == 1) {
 					// Add a student
-					//student student = inputStudent();
-					//C206_CaseStudy.addStudent(studentList, student);
-					//System.out.println("Student added");
+					// student student = inputStudent();
+					// C206_CaseStudy.addStudent(studentList, student);
+					// System.out.println("Student added");
 
 				} else if (userType == 2) {
 					// Add a parent
 					parent p = inputParent();
 					C206_CaseStudy.addParent(parentList, p);
 					System.out.println("Parent added");
-					
+
 				} else if (userType == 3) {
 					// Add a teacher
-					//teacher teacher = inputTeacher();
-					//C206_CaseStudy.addTeacher(teacherList, teacher);
-					//System.out.println("teacher added");
+					// teacher teacher = inputTeacher();
+					// C206_CaseStudy.addTeacher(teacherList, teacher);
+					// System.out.println("teacher added");
 				} else {
 					System.out.println("Invalid type");
-					}
 				}
-			
-				else if (option == OPTION_DELETE) {
-					C206_CaseStudy.setHeader("DELETE");			
-					C206_CaseStudy.itemTypeMenu();
-					
-					int userType = Helper.readInt("Enter option to select user type > ");
+			}
 
-					if (userType == 1) {
-						// Delete a student			
-						//System.out.println("Student deleted");
+			else if (option == OPTION_DELETE) {
+				C206_CaseStudy.setHeader("DELETE");
+				C206_CaseStudy.itemTypeMenu();
 
-					} else if (userType == 2) {
-						// Delete a parent	
-						parent p = inputParent();
-						C206_CaseStudy.deleteParent(parentList, p);
-						System.out.println("Parent deleted");
-						
-					} else if (userType == 3) {
-						// Delete a teacher						
-						//System.out.println("teacher deleted");
-					} else {
-						System.out.println("Invalid type");
-						}
-				
-				} 
-			
-				else if (option == OPTION_QUIT) {
-					System.out.println("Bye!");
-				}
-				else {
-					System.out.println("Invalid option");
+				int userType = Helper.readInt("Enter option to select user type > ");
+
+				if (userType == 1) {
+					// Delete a student
+					// System.out.println("Student deleted");
+
+				} else if (userType == 2) {
+					// Delete a parent
+					parent p = inputParent();
+					C206_CaseStudy.deleteParent(parentList, p);
+					System.out.println("Parent deleted");
+
+				} else if (userType == 3) {
+					// Delete a teacher
+					// System.out.println("teacher deleted");
+				} else {
+					System.out.println("Invalid type");
 				}
 
 			}
-		
-		
-	}
 
-	
+			else if (option == OPTION_QUIT) {
+				System.out.println("Bye!");
+			} else {
+				System.out.println("Invalid option");
+			}
+
+		}
+
+	}
 
 	public static void menu() {
 		C206_CaseStudy.setHeader("RESOURCE CENTRE APP");
@@ -125,61 +122,63 @@ public class C206_CaseStudy {
 		System.out.println(header);
 		Helper.line(80, "-");
 	}
-	
-	//================================= Option 1 View users (CRUD- Read) =================================
-		public static String retrieveAllParent(ArrayList<parent> parentList) {
-			String output = "";
 
-			for (int i = 0; i < parentList.size(); i++) {
-				
-				output += String.format("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "CHILD ID", "CHILD NAME",
-						"CHILD GRADE", "CHILD CLASS","CHILD TEACHER", "NAME", "CCA ID", "EMAIL", "CONTACT NO", parentList.get(i).toString());
+	// ================================= Option 1 View users (CRUD- Read)
+	// =================================
+	public static String retrieveAllParent(ArrayList<parent> parentList) {
+		String output = "";
 
-			}
-			return output;
-		}
-		public static String viewAllParent(ArrayList<parent> parentList) {
-			C206_CaseStudy.setHeader("PARENT LIST");
-			String output = String.format("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "CHILD ID", "CHILD NAME",
-					"CHILD GRADE", "CHILD CLASS","CHILD TEACHER", "NAME", "CCA ID", "EMAIL", "CONTACT NO");
-			 output += retrieveAllParent(parentList);	
-			System.out.println(output);
-			return output;
-		}
-		//does this work?
-		
-	
-		//================================= Option 2 Add an user (CRUD - Create) =================================
-		public static parent inputParent() {
-			int childID = Helper.readInt("Enter child's ID > ");
-			String childName = Helper.readString("Enter child's name > ");
-			char grade = Helper.readChar("Enter child's grade > ");
-			String classroom = Helper.readString("Enter child's class > ");
-			String teacher = Helper.readString("Enter child's teacher > ");
-			String parentName = Helper.readString("Enter parent's name > ");
-			String CCAID = Helper.readString("Enter CCA ID > ");
-			String email = Helper.readString("Enter email > ");
-			int contactNo = Helper.readInt("Enter contact number > ");
+		for (int i = 0; i < parentList.size(); i++) {
 
-			parent p = new parent(childID, childName, grade, classroom, teacher, parentName, CCAID, email, contactNo);
-			return p;
-			
-		}
-		public static void addParent(ArrayList<parent> parentList, parent p) {
-			
-			parentList.add(p);
-			
-		}
-		
-		
-		
-		//================================= Option 3 Delete an user (CRUD - Delete) =================================
-		
-		public static void deleteParent(ArrayList<parent> parentList, parent p) {
+			output += String.format("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "CHILD ID", "CHILD NAME",
+					"CHILD GRADE", "CHILD CLASS", "CHILD TEACHER", "NAME", "CCA ID", "EMAIL", "CONTACT NO",
+					parentList.get(i).toString());
 
-			parentList.remove(p);
-			
 		}
+		return output;
+	}
 
+	public static String viewAllParent(ArrayList<parent> parentList) {
+		C206_CaseStudy.setHeader("PARENT LIST");
+		String output = String.format("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "CHILD ID", "CHILD NAME",
+				"CHILD GRADE", "CHILD CLASS", "CHILD TEACHER", "NAME", "CCA ID", "EMAIL", "CONTACT NO");
+		output += retrieveAllParent(parentList);
+		System.out.println(output);
+		return output;
+	}
+	// does this work?
+
+	// ================================= Option 2 Add an user (CRUD - Create)
+	// =================================
+	public static parent inputParent() {
+		int childID = Helper.readInt("Enter child's ID > ");
+		String childName = Helper.readString("Enter child's name > ");
+		char grade = Helper.readChar("Enter child's grade > ");
+		String classroom = Helper.readString("Enter child's class > ");
+		String teacher = Helper.readString("Enter child's teacher > ");
+		String parentName = Helper.readString("Enter parent's name > ");
+		String CCAID = Helper.readString("Enter CCA ID > ");
+		String email = Helper.readString("Enter email > ");
+		int contactNo = Helper.readInt("Enter contact number > ");
+
+		parent p = new parent(childID, childName, grade, classroom, teacher, parentName, CCAID, email, contactNo);
+		return p;
+
+	}
+
+	public static void addParent(ArrayList<parent> parentList, parent p) {
+
+		parentList.add(p);
+
+	}
+
+	// ================================= Option 3 Delete an user (CRUD - Delete)
+	// =================================
+
+	public static void deleteParent(ArrayList<parent> parentList, parent p) {
+
+		parentList.remove(p);
+
+	}
 
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -11,13 +11,13 @@ public class C206_CaseStudyTest {
 	private parent p2;
 	private student s1;
 	private student s2;
-	
+
 	ArrayList<student> studentList = new ArrayList<student>();
 	ArrayList<parent> parentList = new ArrayList<parent>();
 	ArrayList<Teacher> teacherList = new ArrayList<Teacher>();
 	ArrayList<CCA> CCAList = new ArrayList<CCA>();
 	ArrayList<CCACategory> categoryList = new ArrayList<CCACategory>();
-	
+
 	public C206_CaseStudyTest() {
 		super();
 	}
@@ -27,59 +27,57 @@ public class C206_CaseStudyTest {
 		s1 = new student(1001, "John", 'B', "B101", "Mr Tan");
 		s2 = new student(1002, "Max", 'C', "B102", "Mr Lim");
 		p1 = new parent(1001, "John", 'B', "B101", "Mr Tan", "Mrs Lim", "B2345E", "limsy@gmail.com", 92345678);
-     	p2 = new parent(1002, "Max", 'C', "B102", "Mr Lim", "Mr lee", "A1234D", "leejh@gmail.com", 81234567);
-		
+		p2 = new parent(1002, "Max", 'C', "B102", "Mr Lim", "Mr lee", "A1234D", "leejh@gmail.com", 81234567);
+
 		ArrayList<student> studentList = new ArrayList<student>();
 		ArrayList<parent> parentList = new ArrayList<parent>();
 		ArrayList<Teacher> teacherList = new ArrayList<Teacher>();
 		ArrayList<CCA> CCAList = new ArrayList<CCA>();
 		ArrayList<CCACategory> categoryList = new ArrayList<CCACategory>();
-		
+
 	}
-	
-	
+
 	@Test
 	public void testAddParent() {
 		// Item list is not null, so that can add a new parent
 		assertNotNull("Test if there is valid parent arraylist to add to", parentList);
-		
-		//Given an empty list, after adding 1 parent, the size of the list is 1
-		C206_CaseStudy.addParent(parentList, p1);		
+
+		// Given an empty list, after adding 1 parent, the size of the list is 1
+		C206_CaseStudy.addParent(parentList, p1);
 		assertEquals("Test if that parent arraylist size is 1?", 1, parentList.size());
-		
-		//The parent just added is as same as the first item of the list
+
+		// The parent just added is as same as the first item of the list
 		assertSame("Test that parent is added same as 1st item of the list?", p1, parentList.get(0));
-		
-		//Add another parent. test The size of the list is 2?
+
+		// Add another parent. test The size of the list is 2?
 		C206_CaseStudy.addParent(parentList, p2);
 		assertEquals("Test that parent arraylist size is 2?", 2, parentList.size());
 	}
-	
+
 	@Test
 	public void testViewAllParent() {
 		assertNotNull("Test if there is valid parent arraylist to view from", parentList);
-		
-		String registeredParent= C206_CaseStudy.viewAllParent(parentList);
+
+		String registeredParent = C206_CaseStudy.viewAllParent(parentList);
 		String testOutput = "";
 		assertEquals("Check that ViewAllParentlist", testOutput, registeredParent);
-		
+
 		assertEquals("Test if that parent arraylist size is 2?", 2, parentList.size());
 
 	}
-	
+
 	@Test
 	public void testDeleteParent() {
 		// Item list is not null, so that can delete a parent
 		assertNotNull("Test if there is valid parent arraylist to delete from", parentList);
-		
-		String registeredParent= C206_CaseStudy.viewAllParent(parentList);
+
+		String registeredParent = C206_CaseStudy.viewAllParent(parentList);
 		String testOutput = "";
 		assertEquals("Check that ViewAllParentlist", testOutput, registeredParent);
-		
+
 		C206_CaseStudy.deleteParent(parentList, p2);
 		assertEquals("Test if that parent arraylist size is 1?", 1, parentList.size());
 	}
-	
 
 	@After
 	public void tearDown() throws Exception {
@@ -93,9 +91,5 @@ public class C206_CaseStudyTest {
 		CCAList = null;
 		categoryList = null;
 	}
-
-	
-	
-	
 
 }
